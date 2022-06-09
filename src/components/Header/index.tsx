fff7e4e1e5593ccdc91f5562b3bec40bdcb9ef28
Adson Ventura <adsonventura@gmail.com>
2022-06-09 14:21:45 -0300

    fix: add home link on header logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './header.module.scss';
 import commonStyles from '../../styles/common.module.scss';
 
 export function Header(): JSX.Element {
   return (
     <header className={`${commonStyles.container} ${styles.container}`}>
-      <Image width={239} height={27} src="/images/Logo.svg" alt="ig.news" />
+      <Link href="/">
+        <a href="/">
+          <Image width={239} height={27} src="/images/Logo.svg" alt="ig.news" />
+        </a>
+      </Link>
     </header>
   );
 }
