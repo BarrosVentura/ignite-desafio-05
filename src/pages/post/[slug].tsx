168f23962589fc8a019835c2eb2f1599b0f635af
Adson Ventura <adsonventura@gmail.com>
2022-06-10 14:26:33 -0300

    fix: failed tests and details from post style

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,9 @@
-import { asHTML } from '@prismicio/helpers';
+import { asHTML, asText } from '@prismicio/helpers';
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -10,16 +11,20 @@ import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
+  uid: string;
   first_publication_date: string | null;
   data: {
     title: string;
+    subtitle: string;
     banner: {
       url: string;
     };
     author: string;
     content: {
       heading: string;
-      body: string;
+      body: {
+        text: string;
+      }[];
     }[];
   };
 }
@@ -28,8 +33,26 @@ interface PostProps {
   post: Post;
 }
 
+function getCountedWords(post: Post): number | null {
+  if (!Array.isArray(post?.data.content)) return null;
+  const { content } = post.data;
+
+  const counterLettersFromBody = content.reduce((prevContent, currContent) => {
+    const bodyAsText = asText(currContent.body);
+    const headingAsText = currContent.heading;
+    const fullContent = bodyAsText.concat(headingAsText);
+    const filteredBodyText = fullContent.replace(/[\\n|\n]/gm, '');
+    const lettersLength = filteredBodyText.split(' ').length;
+    return prevContent + lettersLength;
+  }, 0);
+
+  return counterLettersFromBody;
+}
+
 export default function Post({ post }: PostProps): JSX.Element {
   const router = useRouter();
+
+  const counterLettersFromBody = getCountedWords(post);
   if (router.isFallback) {
     return <div>Carregando...</div>;
   }
@@ -42,12 +65,19 @@ export default function Post({ post }: PostProps): JSX.Element {
         <h1>{post.data.title}</h1>
         <div className={styles.detailsContainer}>
           <time>
+            <FiCalendar />
             {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
               locale: ptBR,
             })}
           </time>
-          <span>{post.data.author}</span>
-          <span>tempo de leitura</span>
+          <span>
+            <FiUser />
+            {post.data.author}
+          </span>
+          <span>
+            <FiClock />
+            {Math.ceil(counterLettersFromBody / 200)} min
+          </span>
         </div>
         {post.data.content.map(item => (
           <div key={item.heading}>
@@ -55,7 +85,7 @@ export default function Post({ post }: PostProps): JSX.Element {
             <div
               className={styles.bodyContainer}
               /* eslint-disable-next-line */
-              dangerouslySetInnerHTML={{ __html: item.body }}
+              dangerouslySetInnerHTML={{ __html: asHTML(item.body) }}
             />
           </div>
         ))}
@@ -86,18 +116,21 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const postUid: string = !Array.isArray(params.slug) ? params.slug : '';
   const response = await prismic.getByUID('posts', postUid);
   const { content } = response.data;
+
   const post: Post = {
+    uid: response.uid,
     first_publication_date: response.first_publication_date,
     data: {
-      author: response.data.author,
       title: response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
       banner: {
         url: response.data.banner.url,
       },
       content: content.map(item => {
         return {
+          body: item.body,
           heading: item.heading,
-          body: asHTML(item.body),
         };
       }),
     },
